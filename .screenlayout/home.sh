89abec19861ae3792effb3b03c2f49261f06f8d5
Kazatsu Kazu <kazatsukazu@gmail.com>
2022-03-08 01:06:32 +0100

    screens

---
+++ .screenlayout/home.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# xrandr --output eDP --mode 1920x1080 --pos 0x360 --rotate normal --set TearFree on --output HDMI-A-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --set TearFree on
+xrandr --output eDP --mode 1920x1080 --pos 0x360 --rotate normal --set TearFree on --output HDMI-A-0 --primary --mode 2560x1440 --rate 74.78 --pos 1920x0 --rotate normal --set TearFree on
