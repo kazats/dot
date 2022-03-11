1208496ba3c0e8575a4c7a9868cac1a892aa4993
Kazatsu Kazu <kazatsukazu@gmail.com>
2022-03-11 23:18:24 +0100

    fish: fzf, colors

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -23,7 +23,23 @@ if status is-login
   set -xg NO_AT_BRIDGE      1
   # set -xg QT_STYLE_OVERRIDE "GTK+"
 
-  set -xg FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS "--color=fg:#e0def4,bg:#1f1d2e,hl:#6e6a86 --color=fg+:#908caa,bg+:#191724,hl+:#908caa --color=info:#9ccfd8,prompt:#f6c177,pointer:#c4a7e7 --color=marker:#ebbcba,spinner:#eb6f92,header:#ebbcba"
+
+  set -xg LS_COLORS 'rs=0:di=00;34:ln=00;36:mh=00:pi=40;33:so=00;35:do=00;35:bd=40;33;00:cd=40;33;00:or=31;00:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=00;32:*.tar=00;31:*.tgz=00;31:*.arc=00;31:*.arj=00;31:*.taz=00;31:*.lha=00;31:*.lz4=00;31:*.lzh=00;31:*.lzma=00;31:*.tlz=00;31:*.txz=00;31:*.tzo=00;31:*.t7z=00;31:*.zip=00;31:*.z=00;31:*.dz=00;31:*.gz=00;31:*.lrz=00;31:*.lz=00;31:*.lzo=00;31:*.xz=00;31:*.zst=00;31:*.tzst=00;31:*.bz2=00;31:*.bz=00;31:*.tbz=00;31:*.tbz2=00;31:*.tz=00;31:*.deb=00;31:*.rpm=00;31:*.jar=00;31:*.war=00;31:*.ear=00;31:*.sar=00;31:*.rar=00;31:*.alz=00;31:*.ace=00;31:*.zoo=00;31:*.cpio=00;31:*.7z=00;31:*.rz=00;31:*.cab=00;31:*.wim=00;31:*.swm=00;31:*.dwm=00;31:*.esd=00;31:*.jpg=00;35:*.jpeg=00;35:*.mjpg=00;35:*.mjpeg=00;35:*.gif=00;35:*.bmp=00;35:*.pbm=00;35:*.pgm=00;35:*.ppm=00;35:*.tga=00;35:*.xbm=00;35:*.xpm=00;35:*.tif=00;35:*.tiff=00;35:*.png=00;35:*.svg=00;35:*.svgz=00;35:*.mng=00;35:*.pcx=00;35:*.mov=00;35:*.mpg=00;35:*.mpeg=00;35:*.m2v=00;35:*.mkv=00;35:*.webm=00;35:*.webp=00;35:*.ogm=00;35:*.mp4=00;35:*.m4v=00;35:*.mp4v=00;35:*.vob=00;35:*.qt=00;35:*.nuv=00;35:*.wmv=00;35:*.asf=00;35:*.rm=00;35:*.rmvb=00;35:*.flc=00;35:*.avi=00;35:*.fli=00;35:*.flv=00;35:*.gl=00;35:*.dl=00;35:*.xcf=00;35:*.xwd=00;35:*.yuv=00;35:*.cgm=00;35:*.emf=00;35:*.ogv=00;35:*.ogx=00;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:'
+  # set -xg LS_COLORS 'rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:'
+
+  fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs --processes=\cp
+
+  set -xg FZF_DEFAULT_OPTS    --cycle --layout=reverse --height=50% --border=none --margin=0,1,0,0 \
+                              --preview-window=border-none,nowrap \
+                              "--prompt=''" "--pointer=' ⟩'" "--marker=' ┃'" \
+                              --color=fg:#e0def4,bg:#191724,hl:#c4a7e7 \
+                              --color=fg+:#e0def4:regular,bg+:#26233a,hl+:#c4a7e7:regular:reverse \
+                              --color=prompt:#9ccfd8:regular,pointer:#c4a7e7:regular,marker:#c4a7e7:regular \
+                              --color=spinner:#c4a7e7:regular,info:#555169:regular,header:#c4a7e7:regular \
+                              --color=gutter:#191724,query:#c4a7e7:regular
+  set -U fzf_preview_dir_cmd  exa --icons --group-directories-first --git -TL1
+  set -U fzf_preview_file_cmd bat --style 'changes' --color=always
+  set -U fzf_fd_opts          -H --ignore-file ~/.fdignore
 
 
   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
@@ -62,7 +78,7 @@ if status is-interactive
   alias vd   'nvim -d'
   alias sv   'sudo nvim'
   alias g    'git'
-  alias dot  'git --git-dir=$HOME/.dot --work-tree=$HOME'
+  alias q    'git --git-dir=$HOME/.dot --work-tree=$HOME'
 
   alias e    'exa --icons --group-directories-first --git'
   alias l    'e -TL1'
@@ -101,7 +117,7 @@ if status is-interactive
   alias pmqs 'pacman -Qs'
   alias pmqi 'pacman -Qi'
   alias pmqm 'pacman -Qm'
-  alias pmc  'echo "y\nn\n" | s pacman -Scc'
+  alias pmc  'sudo pacman -Scc'
   alias yy   'rf ~/.cache/paru'
 
   alias za   'zathura'
