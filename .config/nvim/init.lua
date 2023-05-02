99398e7e7be54908c5106c523a289f7907e1e5f4
Kazatsu Kazu <kazatsukazu@gmail.com>
2023-05-02 13:18:08 +0200

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,7 +27,36 @@ require('lazy').setup({
   },
 
   -- -- { 'folke/which-key.nvim', config = true },
-  { 'folke/neodev.nvim',    config = true },
+  -- {
+  --   "folke/noice.nvim",
+  --   config = function()
+  --     require("noice").setup({
+  --       lsp = {
+  --           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --           override = {
+  --             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --             ["vim.lsp.util.stylize_markdown"] = true,
+  --             ["cmp.entry.get_documentation"] = true,
+  --           },
+  --         },
+  --         -- you can enable a preset for easier configuration
+  --         presets = {
+  --           bottom_search = true, -- use a classic bottom cmdline for search
+  --           command_palette = true, -- position the cmdline and popupmenu together
+  --           long_message_to_split = true, -- long messages will be sent to a split
+  --           inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  --           lsp_doc_border = true, -- add a border to hover docs and signature help
+  --         },
+  --     })
+  --   end,
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "rcarriga/nvim-notify",
+  --   }
+  -- },
+  -- { "rcarriga/nvim-notify" },
+
+  { 'folke/neodev.nvim', config = true },
   {
     'folke/trouble.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
@@ -816,6 +845,15 @@ mason_lspconfig.setup_handlers({
           },
         },
       })
+    elseif server_name == 'julials' then
+      lspconfig[server_name].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        root_dir = function(fname)
+          local util = require('lspconfig.util')
+          return util.root_pattern 'Project.toml'(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+        end
+      })
     else
       lspconfig[server_name].setup({
         on_attach = on_attach,
@@ -829,11 +867,12 @@ require('flutter-tools').setup({
   ui = {
     -- the border type to use for all floating windows, the same options/formats
     -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
-    border = 'rounded',
+    border = 'single',
     -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
     -- please note that this option is eventually going to be deprecated and users will need to
     -- depend on plugins like `nvim-notify` instead.
     -- notification_style = 'native' | 'plugin',
+    notification_style = 'plugin'
   },
   decorations = {
     statusline = {
