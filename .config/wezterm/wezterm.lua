4349022706ff6724bf654c380243c93075f18933
Kazatsu Kazu <kazatsukazu@gmail.com>
2023-07-14 09:56:51 +0200

    wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,53 +3,6 @@ local wezterm = require 'wezterm';
 local tab_fg  = '#6e6a86'
 local tab_bg  = '#191724'
 
-local default = {
-  foreground      = "white",    -- [silver] The default text color
-  background      = "black",    -- [black]  The default background color
-  cursor_bg       = "white",    -- [#52ad70] Overrides the cell background color when the current cell is occupied by the cursor and the cursor style is set to Block
-  cursor_fg       = "black",    -- [black]   Overrides the text color when the current cell is occupied by the cursor
-  cursor_border   = "white",    -- [#52ad70] Specifies the border color of the cursor when the cursor style is set to Block, of the color of the vertical or horizontal bar when the cursor style is set to Bar or Underline.
-  selection_fg    = "black",    -- [black]   The foreground color of selected text
-  selection_bg    = "#d0d0d0",  -- [#fffacd] The background color of selected text
-  scrollbar_thumb = "#222222",  -- [#222222] The color of the scrollbar "thumb"; the portion that represents the current viewport
-  split           = "#444444",  -- [#444444] The color of the split lines between panes
-  ansi            = {'#000000', '#c75646', '#8eb33b', '#d0b03c', '#72b3cc', '#c8a0d1', '#218693', '#d0d0d0'}, --{"black", "maroon", "green", "olive",  "navy", "purple",  "teal", "silver"},
-  brights         = {'#5d5d5d', '#e09690', '#cdee69', '#ffe377', '#9cd9f0', '#fbb1f9', '#77dfd8', '#f7f7f7'}, --{"grey",  "red",    "lime",  "yellow", "blue", "fuchsia", "aqua", "white"},
-  -- indexed      = {[136]="#af8700"}, -- Arbitrary colors of the palette in the range from 16 to 255
-  tab_bar = {
-    background = tab_bg,
-
-    active_tab = {
-      bg_color      = '#000000',
-      fg_color      = '#d0d0d0',
-      intensity     = 'Normal', -- "Half" | "Normal" | "Bold"
-      underline     = 'None',   -- "None" | "Single" | "Double"
-      italic        = false,
-      strikethrough = false,
-    },
-
-    inactive_tab = {
-      bg_color = tab_bg,
-      fg_color = tab_fg,
-    },
-
-    inactive_tab_hover = {
-      bg_color = tab_bg,
-      fg_color = tab_fg,
-    },
-
-    new_tab = {
-      bg_color = tab_bg,
-      fg_color = tab_bg,
-    },
-
-    new_tab_hover = {
-      bg_color = tab_bg,
-      fg_color = tab_bg,
-    }
-  }
-}
-
 local rose_pine = {
   foreground      = '#e0def4',
   background      = '#191724',
@@ -101,12 +54,14 @@ return {
   font = wezterm.font_with_fallback {
     'IBM Plex Mono',
     -- 'JetBrains Mono', --/ Fira Code / Iosevka / Cascadia Code
-    'NotoSans Nerd Font',
     'Noto Sans Mono',
+    'Noto Color Emoji',
+    'NotoSans Nerd Font',
     'IBM Plex Sans JP',
     'Noto Sans CJK JP DemiLight',
     'HanaMinA',
     'HanaMinB',
+    'Unifont',
   },
   font_size                                   = 16.0,
   colors                                      = rose_pine,
@@ -136,5 +91,63 @@ return {
       name = 'meer',
       remote_address = 'meer'
     },
-  }
+  },
+
+  keys = {
+    {
+      key = 'u',
+      mods = 'SHIFT|CTRL|ALT',
+      action = wezterm.action.CharSelect {
+        copy_on_select = true,
+        copy_to = 'ClipboardAndPrimarySelection',
+      },
+    }
+  },
 }
+
+-- local default = {
+--   foreground      = "white",    -- [silver] The default text color
+--   background      = "black",    -- [black]  The default background color
+--   cursor_bg       = "white",    -- [#52ad70] Overrides the cell background color when the current cell is occupied by the cursor and the cursor style is set to Block
+--   cursor_fg       = "black",    -- [black]   Overrides the text color when the current cell is occupied by the cursor
+--   cursor_border   = "white",    -- [#52ad70] Specifies the border color of the cursor when the cursor style is set to Block, of the color of the vertical or horizontal bar when the cursor style is set to Bar or Underline.
+--   selection_fg    = "black",    -- [black]   The foreground color of selected text
+--   selection_bg    = "#d0d0d0",  -- [#fffacd] The background color of selected text
+--   scrollbar_thumb = "#222222",  -- [#222222] The color of the scrollbar "thumb"; the portion that represents the current viewport
+--   split           = "#444444",  -- [#444444] The color of the split lines between panes
+--   ansi            = {'#000000', '#c75646', '#8eb33b', '#d0b03c', '#72b3cc', '#c8a0d1', '#218693', '#d0d0d0'}, --{"black", "maroon", "green", "olive",  "navy", "purple",  "teal", "silver"},
+--   brights         = {'#5d5d5d', '#e09690', '#cdee69', '#ffe377', '#9cd9f0', '#fbb1f9', '#77dfd8', '#f7f7f7'}, --{"grey",  "red",    "lime",  "yellow", "blue", "fuchsia", "aqua", "white"},
+--   -- indexed      = {[136]="#af8700"}, -- Arbitrary colors of the palette in the range from 16 to 255
+--   tab_bar = {
+--     background = tab_bg,
+--
+--     active_tab = {
+--       bg_color      = '#000000',
+--       fg_color      = '#d0d0d0',
+--       intensity     = 'Normal', -- "Half" | "Normal" | "Bold"
+--       underline     = 'None',   -- "None" | "Single" | "Double"
+--       italic        = false,
+--       strikethrough = false,
+--     },
+--
+--     inactive_tab = {
+--       bg_color = tab_bg,
+--       fg_color = tab_fg,
+--     },
+--
+--     inactive_tab_hover = {
+--       bg_color = tab_bg,
+--       fg_color = tab_fg,
+--     },
+--
+--     new_tab = {
+--       bg_color = tab_bg,
+--       fg_color = tab_bg,
+--     },
+--
+--     new_tab_hover = {
+--       bg_color = tab_bg,
+--       fg_color = tab_bg,
+--     }
+--   }
+-- }
