84c964aab1a9d5cfc13afc80f35951bfb9c87ef0
Kazatsu Kazu <kazatsukazu@gmail.com>
2023-11-25 10:46:56 +0100

    nvim: fortran dap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1262,6 +1262,38 @@ maps(o_ns, {
 })
 
 local dap = require('dap')
+
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = '/home/v/.local/opt/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+}
+
+dap.configurations.fortran = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+
 maps(o_ns, {
   n = {
     {
