ec2be409028741f0ea1d8656701a149b1215232e
Kazatsu Kazu <kazatsukazu@gmail.com>
2024-09-07 16:12:20 +0200

    nvim: leap -> flash

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -487,11 +487,24 @@ require('lazy').setup({
   'rafamadriz/friendly-snippets',
   'Nash0x7E2/awesome-flutter-snippets',
 
+  -- {
+  --   'ggandor/leap.nvim',
+  --   config = function()
+  --     require('leap').add_default_mappings()
+  --   end,
+  -- },
   {
-    'ggandor/leap.nvim',
-    config = function()
-      require('leap').add_default_mappings()
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
 
   'h-hg/fcitx.nvim',
