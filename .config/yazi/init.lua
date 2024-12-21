fb294a3e29bc92e55712c253fb5e1a083b5758ed
Kazatsu Kazu <kazatsukazu@gmail.com>
2024-12-21 17:33:29 +0100

    ya: 0.4

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -9,3 +9,34 @@ require("bookmarks"):setup({
     },
   },
 })
+
+require("git"):setup()
+THEME.git = THEME.git or {}
+
+THEME.git.modified = ui.Style():fg("blue")
+THEME.git.added = ui.Style():fg("blue")
+THEME.git.untracked = ui.Style():fg("blue")
+THEME.git.ignored = ui.Style():fg("blue")
+THEME.git.deleted = ui.Style():fg("blue")
+THEME.git.updated = ui.Style():fg("blue")
+
+THEME.git.modified_sign = "M"
+THEME.git.added_sign = "A"
+THEME.git.untracked_sign = "?"
+THEME.git.ignored_sign = "I"
+THEME.git.deleted_sign = "D"
+THEME.git.updated_sign = "U"
+
+function Status:name()
+  local h = self._current.hovered
+  if not h then
+    return ""
+  end
+
+  local linked = ""
+  if h.link_to ~= nil then
+    linked = " -> " .. tostring(h.link_to)
+  end
+
+  return " " .. h.name:gsub("\r", "?", 1) .. linked
+end
